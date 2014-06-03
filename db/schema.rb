9f5b6872701d74f8bc404f50ad84fba8e1c194cc
Alex Lien <codefellow@aqlien.com>
2014-06-03 08:48:14 -0700

    switch from admin to general roles

--- db/schema.rb
+++ db/schema.rb
@@ -59,7 +59,7 @@ ActiveRecord::Schema.define(version: 20140602192426) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.boolean  "admin"
+    t.string   "role"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
